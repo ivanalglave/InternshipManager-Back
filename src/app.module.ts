5e5ef00d48e4c59296e96bbdc8e9d89933c2b5a9
Ivan Alglave <ivanalglave@outlook.fr>
2022-11-08 14:17:48 +0100

    added mongodb with docker image and integration in nest server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { mongodb } from './config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot(mongodb.uri)],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
