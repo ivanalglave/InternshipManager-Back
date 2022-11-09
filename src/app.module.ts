8756379f41bb8d193534fedb6c0bfbaaa937aad8
Nabilsenko <96882497+Nabilsenko@users.noreply.github.com>
2022-11-09 22:41:35 +0100

    Initial Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { mongodb } from './config';
+import { PeopleModule } from './people/people.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(mongodb.uri)],
+  imports: [MongooseModule.forRoot(mongodb.uri), AppModule, PeopleModule],
   controllers: [],
   providers: [],
 })
